ca3fe069ad0f95e4daad776846ee3975dfcc454f
Victor Ding <victording@live.com>
2017-04-22 20:55:12 -0400

    Authority Chain

--- contracts/demo.sol
+++ contracts/demo.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.0;
 contract ContractBase
 {
     address public _owner;
-    function ContractBase()
+    function ContractBase() internal
     {
         _owner = msg.sender;
     }
@@ -17,103 +17,115 @@ contract ContractBase
     }
 }
 
-contract Insurer is ContractBase
+contract AuthorityBase is ContractBase
 {
-    struct policy_t
+    function AuthorityBase() internal
+    {}
+
+    mapping (address => bool) authenticated_entities;
+    function ApproveEntity(address entity) _OnlyOwner
     {
-        bool valid;
-        address jurisdiction;
-        string vin;
-        int256 expires;
+        authenticated_entities[entity] = true;
     }
-    string public _name;
-    mapping(bytes32 => policy_t) policies;
-    function Insurer(string name)
+    function DisapproveEntity(address entity) _OnlyOwner
     {
-        _name = name;
+        authenticated_entities[entity] = false;
+    }
+    function IsEntityAuthenticated(address entity) returns (bool)
+    {
+        return authenticated_entities[entity];
     }
 
-    function CreatePolicy(address jurisdiction, string vin) _OnlyOwner
+    address private _supervising_authority;
+    function SupervisingAuthority() returns (address)
     {
-        bytes32 key = hash(vin);
-        if (!policies[key].valid)
-        {
-            // TODO check if jurisdiction is valid
-            policies[key].valid = true;
-            policies[key].jurisdiction = jurisdiction;
-            policies[key].vin = vin;
-        }
-        else
-        {
-            throw;
-        }
+        return _supervising_authority;
     }
-    function RevokePolicy(string vin) _OnlyOwner
+    function UpdateSupervisingAuthority(address addr) internal
     {
-        bytes32 key = hash(vin);
-        if (policies[key].valid)
-        {
-            policies[key].valid = false;
-        }
-        else
-        {
-            throw;
-        }
-    }
-    function UpdatePolicy(string vin, int256 expires) _OnlyOwner
-    {
-        bytes32 key = hash(vin);
-        if (policies[key].valid)
-        {
-            policies[key].expires = expires;
-        }
-        else
-        {
-            throw;
-        }
-    }
-    function QueryPolicy(string vin) returns (address o_jurisdiction, int256 o_expires)
-    {
-        bytes32 key = hash(vin);
-        if (policies[key].valid)
-        {
-            o_jurisdiction = policies[key].jurisdiction;
-            o_expires = policies[key].expires;
-        }
-        else
-        {
-            throw;
-        }
+        _supervising_authority = addr;
     }
 }
 
-contract DMV is ContractBase
+contract Authority_AU is AuthorityBase
+{
+    function Authority_AU()
+    {}
+}
+
+contract Authority_AUQLD is AuthorityBase
+{
+    function Authority_AUQLD(address AuthAU)
+    {
+        UpdateSupervisingAuthority(AuthAU);
+    }
+}
+
+contract Authority_CA is AuthorityBase
+{
+    function Authority_CA()
+    {}
+}
+
+contract Authority_CAON is AuthorityBase
+{
+    function Authority_CAON(address AuthCA)
+    {
+        UpdateSupervisingAuthority(AuthCA);
+    }
+}
+
+contract Authority_US is AuthorityBase
+{
+    function Authority_US()
+    {}
+}
+
+contract Authority_USFL is AuthorityBase
+{
+    function Authority_USFL(address AuthUS)
+    {
+        UpdateSupervisingAuthority(AuthUS);
+    }
+}
+
+contract DMVBase is AuthorityBase
 {
     struct plate_t
     {
         bool valid;
         string number;
         string vin;
-        int256 expires;
+        int256 sticker_expiry_date;
         address insurer;
+        int256 insurance_expiry_date;
     }
 
     // Plate information
     mapping(bytes32 => plate_t) plates;
     mapping(bytes32 => bytes32) vin_to_key;
-    string public _jurisdiction;
-    function DMV(string jurisdiction)
+    function DMVBase() internal
     {
-        _jurisdiction = jurisdiction;
     }
-    function query(bytes32 key) private returns (string o_number, string o_vin, int256 o_expires, address o_insurer)
+    modifier _OnlyApprovedInsurer()
+    {
+        if (!IsApprovedInsurer(msg.sender)) throw;
+        _;
+    }
+    modifier _OwnerOrApprovedInsurer()
+    {
+        if (msg.sender != _owner && !IsApprovedInsurer(msg.sender)) throw;
+        _;
+    }
+
+    function query(bytes32 key) private _OwnerOrApprovedInsurer returns (string o_number, string o_vin, int256 o_sticker_expiry_date, address o_insurer)
     {
         plate_t plate = plates[key];
         if (plate.valid)
         {
             o_number = plate.number;
             o_vin = plate.vin;
-            o_expires = plate.expires;
+            o_sticker_expiry_date = plate.sticker_expiry_date;
             o_insurer = plate.insurer;
         }
         else
@@ -149,40 +161,41 @@ contract DMV is ContractBase
             throw;
         }
     }
-    function UpdateValidity(string plate, int256 expires) _OnlyOwner
+    function UpdateStickerExpiryDate(string plate, int256 sticker_expiry_date) _OnlyOwner
     {
         bytes32 key =  hash(plate);
         if (plates[key].valid)
         {
-            plates[key].expires = expires;
+            plates[key].sticker_expiry_date = sticker_expiry_date;
         }
         else
         {
             throw;
         }
     }
-    function UpdateInsurer(string plate, address insurer) _OnlyOwner
+    function UpdateInsuranceExpireDate(string plate, int256 insurance_expiry_date) _OnlyApprovedInsurer
     {
-        if (IsApprovedInsurer(insurer))
+        bytes32 key =  hash(plate);
+        if (plates[key].valid)
         {
-            bytes32 key =  hash(plate);
-            if (plates[key].valid)
-            {
-                plates[key].insurer = insurer;
-                return;
-            }
+            plates[key].insurer = msg.sender;
+            plates[key].insurance_expiry_date = insurance_expiry_date;
+            return;
         }
-        throw;
     }
-    function QueryNumber(string number) returns (string o_number, string o_vin, int256 o_expires, address o_insurer)
+    function QueryNumber(string number) returns (string o_number, string o_vin, int256 o_sticker_expiry_date, address o_insurer)
     {
         return query(hash(number));
     }
-    function QueryVIN(string vin) returns (string o_number, string o_vin, int256 o_expires, address o_insurer)
+    function QueryVIN(string vin) returns (string o_number, string o_vin, int256 o_sticker_expiry_date, address o_insurer)
     {
         return query(vin_to_key[hash(vin)]);
     }
-
+    function ValidatePlateNumber(string number) returns (bool)
+    {
+        bytes32 key =  hash(number);
+        return plates[key].valid;
+    }
 
     // Insurance information
     mapping (address => bool) insurers;
@@ -194,8 +207,26 @@ contract DMV is ContractBase
     {
         insurers[insurer] = false;
     }
-    function IsApprovedInsurer(address insurer) returns (bool ret)
+    function IsApprovedInsurer(address insurer) returns (bool)
     {
         return insurers[insurer];
     }
+}
+
+contract QueenslandDepartmentOfTransportAndMainRoads is DMVBase
+{
+    function QueenslandDepartmentOfTransportAndMainRoads()
+    {}
+}
+
+contract OntarioMinistryOfTransportation is DMVBase
+{
+    function OntarioMinistryOfTransportation()
+    {}
+}
+
+contract FloridaDepartmentOfMotorVehicles is DMVBase
+{
+    function FloridaDepartmentOfMotorVehicles()
+    {}
 }
